c5d68785101aaf55357d14b6fd8129049ca017b8
Frans Ekman <104152557+fransek@users.noreply.github.com>
2022-07-05 22:10:46 +0200

    API-anrop (test)

--- client/src/components/Global/MovieCard.jsx
+++ client/src/components/Global/MovieCard.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
-const MovieCard = () => {
+const axios = require('axios').default
+
+const MovieCard = (props) => {
+  const [img, setImg] = useState('https://picsum.photos/270/400')
+  const [title, setTitle] = useState('Movie Title (Year)')
+  const apiKey = 'k_yzqb0jra'
+
+  const getMovie = async () => {
+    if (props.idx != 0) return // För att förhindra för många API-anrop
+
+    const response = await axios.get(
+      `https://imdb-api.com/en/API/Title/${apiKey}/${props.movieId}`
+    )
+    console.log(response)
+    setImg(response.data.image)
+    setTitle(response.data.fullTitle)
+  }
+
+  useEffect(() => {
+    getMovie()
+  }, [])
+
   return (
     <div className='movieCard'>
-      <img
-        src='https://picsum.photos/270/400'
-        alt='Movie'
-        className='movieImg'
-      />
+      <img src={img} alt='Movie' className='movieImg' />
       <div className='movieInfo'>
-        <h6>Movie Title (Year)</h6>
+        <h6>{title}</h6>
       </div>
     </div>
   )
--- client/src/components/Global/MovieCardGrid.jsx
+++ client/src/components/Global/MovieCardGrid.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import MovieCard from './MovieCard'
-import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import MovieCard from './MovieCard'
+import React from 'react'
+import Row from 'react-bootstrap/Row'
 
 const MovieCardGrid = (props) => {
+  const movieId = 'tt1375666'
+
   return (
     <div className='popularMoviesGrid'>
       <Row xs={2} sm={3} md={4} lg={5} xl={6} className='g-4'>
         {Array.from({ length: props.length }).map((_, idx) => (
           <Col key={idx}>
-            <MovieCard />
+            <MovieCard idx={idx} movieId={movieId} />
           </Col>
         ))}
       </Row>
